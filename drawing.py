388f31a9ffe22f5189c2df08b42be9469653b58c
Joel <22701252@student.uwa.edu.au>
2020-01-14 22:02:02 +0800

    Implemented the draw_grid function

--- drawing.py
+++ drawing.py
@@ -1,8 +1,25 @@
 import pygame
 import colours
 
+indent = 20
+thickness = 5
+
 def draw_grid(surface):
-    pass
+    dimensions = surface.get_size()
+
+    # Draw vertical lines
+    for x in range(1, 3):
+        pygame.draw.line(surface, colours.black, \
+            (x * (dimensions[0] / 3), indent), \
+            (x * (dimensions[0] / 3), dimensions[1] - indent), \
+            thickness)
+
+    # Draw horizontal lines
+    for y in range(1, 3):
+        pygame.draw.line(surface, colours.black, \
+            (indent, y * (dimensions[1] / 3)), \
+            (dimensions[0] - indent, y * (dimensions[1] / 3)), \
+            thickness)
 
 def draw_cross(surface, x, y):
     pass
