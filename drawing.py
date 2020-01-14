c13748171e1dcdbb5129851d9b2ad781f346f8d4
Joel <22701252@student.uwa.edu.au>
2020-01-14 22:26:29 +0800

    Implemented the draw_cross function

--- drawing.py
+++ drawing.py
@@ -22,6 +22,19 @@ def draw_grid(surface):
             thickness)
 
 def draw_cross(surface, col, row):
+    dimensions = surface.get_size()
+    
+    # Top left to bottom right
+    pygame.draw.line(surface, colours.red, \
+            (indent + (col * (dimensions[0] / 3)), indent + (row * (dimensions[1] / 3))), \
+            (((col + 1) * (dimensions[0] / 3)) - indent, ((row + 1) * (dimensions[1] / 3)) - indent), \
+            thickness)
+    
+    # Top right to bottom left
+    pygame.draw.line(surface, colours.red, \
+            (((col + 1) * (dimensions[0] / 3)) - indent, (row * (dimensions[1] / 3)) + indent), \
+            (indent + (col * (dimensions[0] / 3)), ((row + 1) * (dimensions[1] / 3)) - indent), \
+            thickness)
 
 def draw_nought(surface, col, row):
     pass
