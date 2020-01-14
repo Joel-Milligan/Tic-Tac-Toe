680a9c666dfb4ec8271816b3378f2a0599e1ed64
Joel <22701252@student.uwa.edu.au>
2020-01-14 21:21:05 +0800

    Added colours.py

---
+++ colours.py
@@ -0,0 +1,11 @@
+# GREY SCALE
+black = (0, 0, 0)
+white = (255, 255, 255)
+light_grey = (75, 75, 75)
+medium_grey = (127, 127, 127)
+dark_grey = (200, 200, 200)
+
+# RGB
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
