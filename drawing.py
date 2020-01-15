027fc5f0015f6d7aeec8c79936a86caf5e905847
Joel <22701252@student.uwa.edu.au>
2020-01-15 17:03:50 +0800

    Added window border to drawing

--- drawing.py
+++ drawing.py
@@ -2,7 +2,7 @@ import pygame
 import colours
 
 indent = 20
-thickness = 5
+window_border = 50
 
 def draw_grid(surface, board_state):
     dimensions = surface.get_size()
@@ -10,15 +10,15 @@ def draw_grid(surface, board_state):
     # Draw vertical lines
     for x in range(1, 3):
         pygame.draw.line(surface, colours.black, \
-            (x * (dimensions[0] / 3), indent), \
-            (x * (dimensions[0] / 3), dimensions[1] - indent), \
+            ((x * ((dimensions[0] - (2 * window_border)) / 3)) + window_border, window_border), \
+            ((x * ((dimensions[0] - (2 * window_border)) / 3)) + window_border, dimensions[1] - window_border), \
             thickness)
 
     # Draw horizontal lines
     for y in range(1, 3):
         pygame.draw.line(surface, colours.black, \
-            (indent, y * (dimensions[1] / 3)), \
-            (dimensions[0] - indent, y * (dimensions[1] / 3)), \
+            (window_border, (y * ((dimensions[1] - (2 * window_border)) / 3)) + window_border), \
+            (dimensions[0] - window_border, (y * ((dimensions[1] - (2 * window_border)) / 3)) + window_border), \
             thickness)
 
     # Draw noughts and crosses
@@ -32,23 +32,34 @@ def draw_grid(surface, board_state):
 def draw_cross(surface, col, row):
     dimensions = surface.get_size()
     
+    x11 = (indent + (col * ((dimensions[0] - (2 * window_border)) / 3))) + window_border
+    y11 = (indent + (row * ((dimensions[1] - (2 * window_border)) / 3))) + window_border
+    x12 = (((col + 1) * ((dimensions[0] - (2 * window_border)) / 3)) - indent) + window_border
+    y12 = (((row + 1) * ((dimensions[1] - (2 * window_border)) / 3)) - indent) + window_border
+
     # Top left to bottom right
     pygame.draw.line(surface, colours.red, \
-            (indent + (col * (dimensions[0] / 3)), indent + (row * (dimensions[1] / 3))), \
-            (((col + 1) * (dimensions[0] / 3)) - indent, ((row + 1) * (dimensions[1] / 3)) - indent), \
+            (x11, y11), \
+            (x12, y12), \
             thickness)
     
+    x21 = (((col + 1) * ((dimensions[0] - (2 * window_border)) / 3)) - indent) + window_border
+    y21 = ((row * ((dimensions[1] - (2 * window_border)) / 3)) + indent) + window_border
+    x22 = (indent + (col * ((dimensions[0] - (2 * window_border)) / 3))) + window_border
+    y22 = (((row + 1) * ((dimensions[1] - (2 * window_border)) / 3)) - indent) + window_border
+
     # Top right to bottom left
     pygame.draw.line(surface, colours.red, \
-            (((col + 1) * (dimensions[0] / 3)) - indent, (row * (dimensions[1] / 3)) + indent), \
-            (indent + (col * (dimensions[0] / 3)), ((row + 1) * (dimensions[1] / 3)) - indent), \
+            (x21, y21), \
+            (x22, y22), \
             thickness)
 
 # Currently only works for square window sizes
 def draw_nought(surface, col, row):
     dimensions = surface.get_size()
 
-    pygame.draw.circle(surface, colours.blue, \
-        (int((col * dimensions[0] / 3) + dimensions[0] / 6), \
-        int((row * dimensions[1] / 3) + dimensions[1] / 6)), \
-        int((dimensions[0] / 6) - indent), int(thickness))
+    x = (col * (dimensions[0] - (2 * window_border)) / 3) + ((dimensions[0] - (2 * window_border)) / 6) + window_border
+    y = (row * (dimensions[1] - (2 * window_border)) / 3) + ((dimensions[1] - (2 * window_border)) / 6) + window_border 
+    r = ((dimensions[0] - (2 * window_border)) / 6) - indent
+
+    pygame.draw.circle(surface, colours.blue, (int(x), int(y)), int(r), int(thickness))
